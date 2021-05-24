c146ad1f63b44c93420b09357f0dbc656c039de0
chagmn <sopg4@naver.com>
2021-05-24 15:54:40 +0900

    Initial Commit

---
+++ SwiftUI Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI Study
+//
+//  Created by 창민 on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
