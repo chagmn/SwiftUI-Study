661d9589919f3314e912f92840302aa906dd68f0
chagmn <sopg4@naver.com>
2021-06-07 17:53:12 +0900

    update TextFieldStudy

--- SwiftUI Study/TextFieldStudy.swift
+++ SwiftUI Study/TextFieldStudy.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct TextFieldStudy: View {
     @State private var textFielddata: String = ""
-    @State private var withPicker: String = ""
+    @State private var keyboardText: String = ""
+
+    
+    var numberFormatter: NumberFormatter{
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }
     
     var body: some View {
         VStack(alignment: .center, spacing: 20){
+            
+            Text("\(textFielddata)")
+                .frame(width: 320, height: 40)
+                .background(Color.init(.systemGray6))
+            
             TextField(" 텍스트 필드", text: $textFielddata)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.black)
@@ -20,13 +32,20 @@ struct TextFieldStudy: View {
                 .background(Color(#colorLiteral(red: 0.8564875722, green: 0.8513966799, blue: 0.8604011536, alpha: 1)))
                 .cornerRadius(15)
             
-            TextField("Picker와 연동용", text: $withPicker)
+            
+            // formatter를 설정해서 숫자를 표시할 수도 있음.
+            TextField("키보드", value: $keyboardText, formatter: numberFormatter)
                 .foregroundColor(.black)
-                .frame(width: 150, height: 40)
-                
+                .frame(width: 320, height: 40)
                 .cornerRadius(10)
                 .border(Color.black, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                
+                .keyboardType(.numberPad) // 이걸로 상황에 따라 키보드 형태를 바꿀 수 있음
+            
+            // .numberPad 경우 리턴버튼이 없어서 키보드를 못내리는데 이때 이 버튼을 이용해서
+            // 키보드를 내릴 수 있음.
+            Button("키보드 내리기"){
+                self.hideKeyboard()
+            }
         }
     }
 }
@@ -34,5 +53,12 @@ struct TextFieldStudy: View {
 struct TextFieldStudy_Previews: PreviewProvider {
     static var previews: some View {
         TextFieldStudy()
+    }
+}
+
+extension View{
+    // 키보드 내리는 함수
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
