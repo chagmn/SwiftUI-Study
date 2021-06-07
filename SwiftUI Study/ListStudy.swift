9d20a87f01338ab283affb4d2cbf2aee600e0a5c
chagmn <sopg4@naver.com>
2021-06-07 18:36:31 +0900

    update ListStudy - add move, delete, select

--- SwiftUI Study/ListStudy.swift
+++ SwiftUI Study/ListStudy.swift
@@ -8,29 +8,48 @@
 import SwiftUI
 
 struct ListStudy: View {
-    let one: [String] = ["가", "나", "다", "라"]
-    let two: [String] = ["마", "바", "사","아","자"]
+    @State var select = Set<UUID>()
+    
+    @State var one: [String] = ["가", "나", "다", "라"]
+    var two: [String] = ["마", "바", "사","아","자"]
     
     var body: some View {
         let titles: [String] = ["1st Section", "2nd Section"]
         let data = [one, two]
         
-        return List {
-            ForEach(one, id: \.self){ value in
-                if value == "다" {
-                    Text("\(value)!!!")
+        NavigationView{
+            List(selection: $select) {
+                ForEach(one, id: \.self){ value in
+                    if value == "다" {
+                        Text("\(value)!!!")
+                    }
+                    Text("\(value) 리스트")
                 }
-                Text("\(value) 리스트")
-            }
-            ForEach(data.indices) { index in
-                Section(header: Text(titles[index]), footer: HStack{Spacer();Text("\(data[index].count)개")})
-                {
-                    ForEach(data[index], id: \.self){
-                        Text($0)
+                .onDelete(perform: removeList)
+                .onMove(perform: moveList)
+                ForEach(data.indices) { index in
+                    Section(header: Text(titles[index]), footer: HStack{Spacer();Text("\(data[index].count)개")})
+                    {
+                        ForEach(data[index], id: \.self){
+                            Text($0)
+                        }
                     }
                 }
+            }.listStyle(SidebarListStyle())
+            // .listStyle(InsetGroupedListStyle())
+            //
+            .navigationTitle("Title")
+            .toolbar{
+                EditButton()
             }
-        }.listStyle(InsetGroupedListStyle())
+        }
+    }
+    func removeList(at offset: IndexSet){
+        one.remove(atOffsets: offset)
+    }
+    
+    func moveList(from source: IndexSet, to direction: Int){
+        one.move(fromOffsets: source, toOffset: direction)
     }
 }
 
