19b094b0769bbcfb2b7e0579b91eb115d47bc71a
chagmn <sopg4@naver.com>
2021-06-14 17:36:56 +0900

    add Wifi View

---
+++ SwiftUI Study/Views/Setting/Wifi.swift
@@ -0,0 +1,20 @@
+//
+//  Wifi.swift
+//  SwiftUI Study
+//
+//  Created by 창민 on 2021/06/14.
+//
+
+import SwiftUI
+
+struct Wifi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Wifi_Previews: PreviewProvider {
+    static var previews: some View {
+        Wifi()
+    }
+}
