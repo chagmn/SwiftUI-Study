23de37e22fc31e39e97061d07f81859155eb539d
chagmn <sopg4@naver.com>
2021-06-14 17:24:09 +0900

    update AppleID View

--- SwiftUI Study/Views/Setting/AppleID.swift
+++ SwiftUI Study/Views/Setting/AppleID.swift
@@ -10,11 +10,9 @@ import SwiftUI
 struct AppleID: View {
     var body: some View {
         List {
-            
             Section(header: header()){
                 NavigationLink(destination: destination_example() ){
                     Text("이름, 전화번호, 이메일")
-                    
                 }
 
                 NavigationLink(destination: destination_example() ){
@@ -22,6 +20,9 @@ struct AppleID: View {
                 }
                 NavigationLink(destination: destination_example() ){
                     Text("결제 및 배송")
+                    Spacer()
+                    Text("휴대 전화번호")
+                        .foregroundColor(Color.init(.systemGray))
                 }
                 NavigationLink(destination: destination_example() ){
                     Text("구독")
@@ -50,16 +51,29 @@ struct destination_example: View{
 }
 
 struct header: View{
+    @State private var isImageChange: Bool = false
     var body: some View{
         
         VStack(alignment: .center, spacing: 3){
             Button(action: {
-                
+                self.isImageChange.toggle()
             }){
-                Image(systemName: "person.crop.circle.fill")
+                Image(systemName: "person.fill")
                     .resizable()
-                    .foregroundColor(.black)
-                    .frame(width: 80, height: 80)
+                    .foregroundColor(.white)
+                    .frame(width: 60, height: 60)
+                    .padding(.all, 10)
+                    .overlay(
+                        Text("편집")
+                            .frame(width: 100, height: 20)
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                        , alignment: .bottom
+                    )
+                    .background(Color.init(UIColor.systemGray4))
+                    .clipShape(Circle())
+                    .actionSheet(isPresented: $isImageChange, content: getActionSheet)
+                    
             }
             
             Text("이사과")
@@ -75,6 +89,16 @@ struct header: View{
         .frame(width: UIScreen.main.bounds.width,alignment: .center)
     }
     
+    func getActionSheet() -> ActionSheet{
+        let title = Text("선택해주세요.")
+        
+        let takeImage: ActionSheet.Button = .default(Text("사진 찍기"))
+        let chooseImage: ActionSheet.Button = .default(Text("사진 선택"))
+        let searching: ActionSheet.Button = .default(Text("탐색..."))
+        let cancle: ActionSheet.Button = .cancel(Text("취소"))
+        
+        return ActionSheet(title: title, buttons: [takeImage,chooseImage,searching,cancle])
+    }
 }
 struct AppleID_Previews: PreviewProvider {
     static var previews: some View {
